de6fc518e5b580b69a9bc7199e73d155cdabc3eb
Hugo Trippaers <htrippaers@schubergphilis.com>
2025-02-11 22:06:05 +0100

    Fixes for the build pipeline

--- Inc/hexapod.h
+++ Inc/hexapod.h
@@ -12,7 +12,7 @@ extern "C" {
 #endif
 
 
-void project_point_on_circle(float32_t radius, const float32_t origin[3], const float32_t direction[3], float32_t point[3]);
+void project_point_on_circle(float32_t radius, const float32_t origin[2], const float32_t direction[2], float32_t point[2]);
 uint32_t almost_equal(float32_t srcA, float32_t srcB, float32_t epsilon);
 void calculate_path(float32_t current[3], float32_t target[3], float32_t lift_height, float32_t lift_incline_factor, float32_t path[4][3]);
 void interpolate(float32_t path[4][3], float32_t step_size, float32_t next[3]);
