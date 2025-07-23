671232c15a23e35ff8742c0908426384c14f176d
Hugo Trippaers <htrippaers@schubergphilis.com>
2025-07-23 16:20:07 +0200

    Include math.h for M_PI

--- src/inverse_kinematics.c
+++ src/inverse_kinematics.c
@@ -1,6 +1,8 @@
 #include "hexapodmath/inverse_kinematics.h"
 #include "hexapodmath/additional_functions.h"
 
+#include "math.h"
+
 void inverse_kinematics(const float32_t *origin, const float32_t *tip, float32_t *angles)
 {
     // TODO This should come from a robot definition file
